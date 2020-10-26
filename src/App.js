7fb484cc353bb7cd3794654c06770ebc3b672b40
Joshua Small <jmsmall89@gmail.com>
2020-10-26 09:09:42 -0400

    +BASENAME URL

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // CSS3
-import './css/App.css';
+import "./css/App.css";
 // COMPONENTS
-import NavBar from './components/NavBar';
-import Jumbotron from './components/Jumbotron';
+import NavBar from "./components/NavBar";
+import Jumbotron from "./components/Jumbotron";
 import MainPage from "./containers/MainPage";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 
 // APP
 function App() {
   return (
-    <Router basename="hw-19-employee-directory">
+    <Router basename={process.env.PUBLIC_URL}>
       <NavBar />
       <Jumbotron />
       <Route exact path="/" component={MainPage} />
